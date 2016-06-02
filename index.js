0a39af1d9270d34dc8de6d86544bdedbedd14ce6
Daniel White <danbrianwhite@gmail.com>
2016-06-02 15:49:16 -0400

    Update add operation to allow setting object instead of default array

--- index.js
+++ index.js
@@ -3,25 +3,13 @@ var toDot = require('jsonpath-to-dot');
 module.exports = function(patches){
   var update = {};
   patches.map(function(p){
-    if(p.op === 'add'){
-      var path = toDot(p.path);
-      if(!update.$push) update.$push = {};
-      if(!update.$push[path]) {
-        update.$push[path] = p.value;
-      } else if (update.$push[path]) {
-        if(!update.$push[path].$each) {
-          update.$push[path] = {$each : [update.$push[path]]};
-        }
-        update.$push[path].$each.push(p.value);
-      }
+    if(p.op === 'add' || p.op === 'replace'){
+      if(!update.$set) update.$set = {};
+      update.$set[toDot(p.path)] = p.value;
     }
     else if(p.op === 'remove'){
       if(!update.$unset) update.$unset = {};
       update.$unset[toDot(p.path)] = 1;
-    }
-    else if(p.op === 'replace'){
-      if(!update.$set) update.$set = {};
-      update.$set[toDot(p.path)] = p.value;
     }
     else if(p.op !== 'test') {
       throw new Error('Unsupported Operation! op = ' + p.op);
