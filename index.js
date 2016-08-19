3cff2bc48b09111a4fa82dbd7a8e7d5cc36367fa
Daniel White <danbrianwhite@gmail.com>
2016-08-19 16:01:51 -0400

    Add option to pass isArray to command
    Add option to pass isArray to command

--- index.js
+++ index.js
@@ -2,17 +2,30 @@ var toDot = require('jsonpath-to-dot');
 
 module.exports = function(patches){
   var update = {};
+
   patches.map(function(p){
     if(p.op === 'add' || p.op === 'replace'){
       if(!update.$set) update.$set = {};
       update.$set[toDot(p.path)] = p.value;
     }
     else if(p.op === 'remove'){
-      if(!update.$unset) update.$unset = {};
-      if(!update.$pull) update.$pull = {};
+
+      var isArray = !p.hasOwnProperty('isArray') ? true : p.isArray;
+
+      if(!update.$unset){
+        update.$unset = {};
+      }
       update.$unset[toDot(p.path)] = 1;
-      var pullPath = toDot(p.path).replace(/\.[0-9]*$/, '');
-      update.$pull[pullPath] = null;
+      if(isArray)
+      {
+        if(!update.$pull) {
+          update.$pull = {};
+        }
+
+          var pullPath = toDot(p.path).replace(/\.[0-9]*$/, '');
+          update.$pull[pullPath] = null;
+      }
+
     }
     else if(p.op !== 'test') {
       throw new Error('Unsupported Operation! op = ' + p.op);
