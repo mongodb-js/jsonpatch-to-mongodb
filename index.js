50fb41f64231cb524108a753acf7a84e90be0208
Dan White <danbrianwhite@gmail.com>
2017-06-02 22:13:28 -0400

    Match code to current latest.

--- index.js
+++ index.js
@@ -4,18 +4,51 @@ module.exports = function(patches){
   var update = {};
 
   patches.map(function(p){
-    if(p.op === 'add' || p.op === 'replace'){
-        var path = toDot(p.path);
-        if(!update.$set) update.$set = {};
-        if(!update.$set[path]) {
-            update.$set[path] = p.value;
-        } else if (update.$set[path]) {
-            if(!update.$set[path].$each) {
-                update.$set[path] = {$each : [update.$set[path]]};
-            }
-            update.$set[path].$each.push(p.value);
-        }
-    }
+      if(p.op === 'add' || p.op === 'replace'){
+          var path = toDot(p.path),
+              parts = path.split('.');
+
+          var key = parts[0];
+          var $position = parts[1] && parseInt(parts[1], 10);
+
+          update.$set = update.$set || {};
+
+          if (!isNaN($position)) {
+              if (update.$set[key]) {
+                  if (!isNaN(update.$set[key].$position)) {
+                      $position = update.$set[key].$position;
+                      delete update.$set[key].$position;
+                  }
+
+                  if (!update.$set[key].$each) {
+                      update.$set[key] = {
+                          $each: [
+                              update.$set[key]
+                          ]
+                      };
+                  }
+
+                  update.$set[key].$each.push(p.value);
+                  update.$set[key].$position = $position;
+              } else {
+                  update.$set[key] = {
+                      $each: [p.value],
+                      $position: $position
+                  };
+              }
+          } else {
+              if (update.$set[key]) {
+                  if (!update.$set[key].$each) {
+                      update.$set[key] = {
+                          $each: [update.$set[key]]
+                      };
+                  }
+                  update.$set[path].$each.push(p.value);
+              } else {
+                  update.$set[path] = p.value;
+              }
+          }
+      }
     else if(p.op === 'remove'){
 
       var isArray = !p.hasOwnProperty('isArray') ? true : p.isArray;
--- test/index.test.js
+++ test/index.test.js
@@ -4,137 +4,183 @@ var chai = require('chai');
 
 describe('jsonpatch to mongodb', function() {
 
-  it('should work with single add', function() {
-    var patches = [{
-      op: 'add',
-      path: '/name',
-      value: 'dave'
-    }];
+    it('should work with single add', function() {
+        var patches = [{
+            op: 'add',
+            path: '/name',
+            value: 'dave'
+        }];
 
-    var expected = {
-      $set: {
-        name: 'dave'
-      }
-    };
+        var expected = {
+            $set: {
+                name: 'dave'
+            }
+        };
 
-    assert.deepEqual(toMongodb(patches), expected);
-  });
+        assert.deepEqual(toMongodb(patches), expected);
+    });
 
-  it('should work with multiple adds', function() {
-    var patches = [{
-      op: 'add',
-      path: '/name',
-      value: 'dave'
-    },{
-      op: 'add',
-      path: '/name',
-      value: 'bob'
-    },{
-      op: 'add',
-      path: '/name',
-      value: 'john'
-    }];
+    it('should work with array set', function() {
+        var patches = [{
+            op: 'add',
+            path: '/name/1',
+            value: 'dave'
+        }];
 
-    var expected = {
-      $set: {
-        name: {$each: ['dave', 'bob', 'john']}
-      }
-    };
+        var expected = {
+            $set: {
+                name: {
+                    $each: [
+                        'dave'
+                    ],
+                    $position: 1
+                }
+            }
+        };
 
-    assert.deepEqual(toMongodb(patches), expected);
-  });
+        assert.deepEqual(toMongodb(patches), expected);
+    });
 
-  it('should work with remove', function() {
-    var patches = [{
-      op: 'remove',
-      path: '/name',
-      value: 'dave'
-    }];
+    it('should work with multiple set', function() {
+        var patches = [{
+            op: 'add',
+            path: '/name/1',
+            value: 'dave'
+        }, {
+            op: 'add',
+            path: '/name/2',
+            value: 'bob'
+        }];
 
-    var expected = {
-      $unset: {
-        name: 1
-      },
-      $pull: {
-        name: null
-      }
-    };
+        var expected = {
+            $set: {
+                name: {
+                    $each: [
+                        'dave',
+                        'bob'
+                    ],
+                    $position: 1
+                }
+            }
+        };
 
-    assert.deepEqual(toMongodb(patches), expected);
-  });
+        assert.deepEqual(toMongodb(patches), expected);
+    });
 
-  it('should work with replace', function() {
-    var patches = [{
-      op: 'replace',
-      path: '/name',
-      value: 'dave'
-    }];
+    it('should work with multiple adds', function() {
+        var patches = [{
+            op: 'add',
+            path: '/name',
+            value: 'dave'
+        },{
+            op: 'add',
+            path: '/name',
+            value: 'bob'
+        },{
+            op: 'add',
+            path: '/name',
+            value: 'john'
+        }];
 
-    var expected = {
-      $set: {
-        name: 'dave'
-      }
-    };
+        var expected = {
+            $set: {
+                name: {$each: ['dave', 'bob', 'john']}
+            }
+        };
 
-    assert.deepEqual(toMongodb(patches), expected);
-  });
+        assert.deepEqual(toMongodb(patches), expected);
+    });
 
-  it('should work with multiple replace', function() {
-    var patches = [{
-        op: 'replace',
-        path: '/name',
-        value: 'dave'
-    },{
-        op: 'replace',
-        path: '/name',
-        value: 'bob'
-    },{
-        op: 'replace',
-        path: '/name',
-        value: 'john'
-    }];
+    it('should work with remove', function() {
+        var patches = [{
+            op: 'remove',
+            path: '/name',
+            value: 'dave'
+        }];
 
-    var expected = {
-        $set: {
-            name: {$each: ['dave', 'bob', 'john']}
-        }
-    };
+        var expected = {
+            $unset: {
+                name: 1
+            },
+            $pull: {
+                name: null
+            }
+        };
 
-    assert.deepEqual(toMongodb(patches), expected);
-  });
+        assert.deepEqual(toMongodb(patches), expected);
+    });
 
-  it('should work with test', function() {
-    var patches = [{
-      op: 'test',
-      path: '/name',
-      value: 'dave'
-    }];
+    it('should work with replace', function() {
+        var patches = [{
+            op: 'replace',
+            path: '/name',
+            value: 'dave'
+        }];
 
-    var expected = {};
+        var expected = {
+            $set: {
+                name: 'dave'
+            }
+        };
 
-    assert.deepEqual(toMongodb(patches), expected);
-  });
+        assert.deepEqual(toMongodb(patches), expected);
+    });
 
-  it('should blow up on move', function() {
-    var patches = [{
-      op: 'move',
-      path: '/name',
-      from: '/old_name'
-    }];
+    it('should work with multiple replaces', function() {
+        var patches = [{
+            op: 'replace',
+            path: '/name',
+            value: 'dave'
+        },{
+            op: 'replace',
+            path: '/name',
+            value: 'bob'
+        },{
+            op: 'replace',
+            path: '/name',
+            value: 'john'
+        }];
 
-    chai.expect(function(){toMongodb(patches)}).to.throw('Unsupported Operation! op = move');
+        var expected = {
+            $set: {
+                name: {$each: ['dave', 'bob', 'john']}
+            }
+        };
 
-  });
+        assert.deepEqual(toMongodb(patches), expected);
+    });
+
+    it('should work with test', function() {
+        var patches = [{
+            op: 'test',
+            path: '/name',
+            value: 'dave'
+        }];
+
+        var expected = {};
+
+        assert.deepEqual(toMongodb(patches), expected);
+    });
+
+    it('should blow up on move', function() {
+        var patches = [{
+            op: 'move',
+            path: '/name',
+            from: '/old_name'
+        }];
+
+        chai.expect(function(){toMongodb(patches)}).to.throw('Unsupported Operation! op = move');
+
+    });
 
 
-  it('should blow up on copy', function() {
-    var patches = [{
-      op: 'copy',
-      path: '/name',
-      from: '/old_name'
-    }];
+    it('should blow up on copy', function() {
+        var patches = [{
+            op: 'copy',
+            path: '/name',
+            from: '/old_name'
+        }];
 
-    chai.expect(function(){toMongodb(patches)}).to.throw('Unsupported Operation! op = copy');
-  });
+        chai.expect(function(){toMongodb(patches)}).to.throw('Unsupported Operation! op = copy');
+    });
 });
-
